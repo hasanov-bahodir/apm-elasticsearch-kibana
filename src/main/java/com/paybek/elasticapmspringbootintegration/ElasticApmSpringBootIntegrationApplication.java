d66cc58b4efc55ea29d067d8741f547b0accd3b8
hasanov-bahodir <hasanovbahodirqa@gmail.com>
2022-12-20 17:07:26 +0500

    inial commit

---
+++ src/main/java/com/paybek/elasticapmspringbootintegration/ElasticApmSpringBootIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.paybek.elasticapmspringbootintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticApmSpringBootIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticApmSpringBootIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paybek/elasticapmspringbootintegration/ElasticApmSpringBootIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paybek.elasticapmspringbootintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticApmSpringBootIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
